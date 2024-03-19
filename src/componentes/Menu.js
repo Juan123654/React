149bd0486671028f196ff75c6334d3725aefed0c
Juan123654 <jplsslon@hotmail.com>
2024-03-19 08:58:35 -0500

    funcional ingreso de imagen y cambio css

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -1,20 +1,32 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../css/Menu.css";
-function Menu(){
-    return(
-        <div className="menu">
-          <div>
-            <h1>LOGO</h1>
-          </div>
-          <div>
-          <ul>
-            <li><Link className="item" to="/home">Home</Link></li>
-            <li><Link className="item" to="/registro">Registro Actividad</Link></li>
-          </ul>  
-          </div>        
-       </div>
-    );
+function Menu() {
+  return (
+    <div className="menu">
+      <div>
+        <img
+          className="imagen"
+          src={require("../img/img.jpg")}
+          alt="imagen 1"
+        />
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link className="item" to="/home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="item" to="/registro">
+              Registro Actividad
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
